ce21a3db80d8e29659284d0d208087080ad20e0d
Luke Spragg <luke@spragg.me>
2017-11-25 12:54:02 -0500

    Initial commit of split/migrated base

---
+++ src/HurtworldPluginLoader.cs
@@ -0,0 +1,13 @@
+﻿using Oxide.Core.Plugins;
+using System;
+
+namespace Oxide.Game.Hurtworld
+{
+    /// <summary>
+    /// Responsible for loading Hurtworld core plugins
+    /// </summary>
+    public class HurtworldPluginLoader : PluginLoader
+    {
+        public override Type[] CorePlugins => new[] { typeof(HurtworldCore) };
+    }
+}
