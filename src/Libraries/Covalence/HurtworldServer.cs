32c2bb52bbddb99361b6b4e3b68708dc14da645c
Luke Spragg <luke@spragg.me>
2018-07-02 13:51:13 -0400

    Tweak Covalence server.Address handling, add debug build output

--- src/Libraries/Covalence/HurtworldServer.cs
+++ src/Libraries/Covalence/HurtworldServer.cs
@@ -3,7 +3,6 @@ using Oxide.Core.Libraries.Covalence;
 using System;
 using System.Globalization;
 using System.Net;
-using uLink;
 
 namespace Oxide.Game.Hurtworld.Libraries.Covalence
 {
@@ -45,15 +44,33 @@ namespace Oxide.Game.Hurtworld.Libraries.Covalence
                     {
                         try
                         {
-                            if (Utility.ValidateIPv4(Network.config.localIP) && !Utility.IsLocalIP(Network.config.localIP))
+                            if (Utility.ValidateIPv4(GameManager.Instance.ServerConfig.BoundIP) && !Utility.IsLocalIP(GameManager.Instance.ServerConfig.BoundIP))
                             {
-                                IPAddress.TryParse(Network.config.localIP, out address);
+                                IPAddress.TryParse(GameManager.Instance.ServerConfig.BoundIP, out address);
+#if DEBUG
+                                Interface.Oxide.LogWarning($"IP address from command-line: {address}");
+#endif
+                            }
+                            else
+                            {
+                                uint ip = Steamworks.SteamGameServer.GetPublicIP();
+                                if (ip > 0)
+                                {
+                                    string publicIp = string.Concat(ip >> 24 & 255, ".", ip >> 16 & 255, ".", ip >> 8 & 255, ".", ip & 255);
+                                    IPAddress.TryParse(publicIp, out address);
+#if DEBUG
+                                    Interface.Oxide.LogWarning($"IP address from Steam query: {address}");
+#endif
+                                }
                             }
                         }
                         catch
                         {
                             WebClient webClient = new WebClient();
                             IPAddress.TryParse(webClient.DownloadString("http://api.ipify.org"), out address);
+#if DEBUG
+                            Interface.Oxide.LogWarning($"IP address from external API: {address}");
+#endif
                         }
                     }
 
